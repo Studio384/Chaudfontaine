992e7dc0e3bde063a83bb27d325c6d12168f1ea7
Devilquake - Joren Martens <joren.martens135@gmail.com>
2017-03-18 17:16:42 +0100

    - Deleted index files - Added file upload with AES encryption - Started RSA

---
+++ application/models/RSA_Encryption.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: 11400277
+ * Date: 18/03/2017
+ * Time: 16:29
+ */
+class RSA_Encryption
+{
+    public function generate_keys()
+    {
+
+    }
+
+    public function encrypt()
+    {
+
+    }
+
+    public function decrypt()
+    {
+
+    }
+}
